9feaf6567e0df46d652b3dd3a00b9b9afc48737a
Paul Brady <pablo.brady@gmail.com>
2015-10-23 11:13:43 -0700

    [Add] Site linked added to readme. [Refactor] Google Analytics tracking. [Refactor] Other unrelated tweaks.

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
-# ocean_soundscapes
+# Ocean Soundscapes
 Distraction-free Audio
 
 
-##### Tech:
+
+##### Tech Stack (aka. '*back to the basics*'):
+
 * Javascript
 * jQuery
 * HTML5 Audio
+
+
+#####*Coming soon:*
+- Faster page startup / audio downloads
+- (Vast) Interface improvements
+
+
+
+##### Live Demo
+
+Can't wait?  Me neither! Try this...<br>
+[http://www.oceansoundscapes.com](http://www.oceansoundscapes.com)
+
+
+
+
 
 
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var app = express();
 var router = require('./server/router');
 
 //set port variable to deployed port or 8000 for local host
-var port = process.env.PORT || 8000;
+var port = normalizePort( process.env.PORT || 8000 );
+
+/**
+ * Normalizes a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 
 //set dirname to client folder to serve static assets (index.html)
 app.use('/', express.static(__dirname + '/public'));
